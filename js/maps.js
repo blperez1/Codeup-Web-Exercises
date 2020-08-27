dd078beae63beca095d12955461f9ad3064d2d25
Blossom Perez <blossomperez18@gmail.com>
2020-08-27 15:58:42 -0500

    added more popups

--- js/maps.js
+++ js/maps.js
@@ -38,38 +38,5 @@ locations.forEach(function (location) {
 
 
 
-// const riverCenterPin = new mapboxgl.Marker()
-//     .setLngLat([-98.4843, 29.4359])
-//     .addTo(map)
-
-// console.table(riverCenterPin)
-
-// const alamoPin = new mapboxgl.Marker()
-//     .setLngLat([-98.485204, 29.429790])
-//     .addTo(map)
-
-// const historicalParkPin = new mapboxgl.Marker()
-//     .setLngLat([-98.4861, 29.4260])
-//     .addTo(map)
-
-// const riverCenter = new mapboxgl.Popup()
-//     .setHTML("<p>River Center Mall</p>")
-
-
-
-// const historicalPark = new mapboxgl.Popup()
-//     .setHTML("<p> San Antonio Missions National Historical Park</p>")
-
-
-
-// const alamo = new mapboxgl.Popup()
-//     .setHTML("<p>Alamo</p>")
-
-// riverCenterPin.setPopup(riverCenter);
-// historicalParkPin.setPopup(historicalPark);
-// alamoPin.setPopup(alamo)
-
-
-
 
 
