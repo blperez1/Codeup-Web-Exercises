e71c94dc6a6eeb10a72dff1d5708deb20d59a14a
Blossom Perez <blossomperez18@gmail.com>
2020-08-27 16:27:16 -0500

    Refactored code into an object and forEach

--- js/maps.js
+++ js/maps.js
@@ -8,35 +8,34 @@ let map = new mapboxgl.Map({
     zoom: 13// starting zoom
 });
 
-const riverCenterPin = new mapboxgl.Marker()
-    .setLngLat([-98.4843, 29.4359])
-    .addTo(map)
 
-const alamoPin = new mapboxgl.Marker()
-    .setLngLat([-98.485204, 29.429790])
-    .addTo(map)
+let locations = [
+    {
+        lngLat: [-98.4843, 29.4359],
+        name: "Rivercenter Mall",
+        address: "849 E Commerce St, San Antonio, TX 78205"
+    },
+    {
+        lngLat: [-98.485204, 29.429790],
+        name: "The Alamo",
+        address: "300 Alamo Plaza, San Antonio, TX 78205"
 
-const historicalParkPin = new mapboxgl.Marker()
-    .setLngLat([-98.4861, 29.4260])
-    .addTo(map)
+    },
+    {
+        lngLat: [-98.4861, 29.4260],
+        name: "San Antonio Missions National Historical Park",
+        address: "6701 San Jose Dr, San Antonio, TX 78214"
+    }
+]
 
-const riverCenter = new mapboxgl.Popup()
-    .setHTML("<p>River Center Mall</p>")
-
-
-
-const historicalPark = new mapboxgl.Popup()
-    .setHTML("<p> San Antonio Missions National Historical Park</p>")
-
-
-
-const alamo = new mapboxgl.Popup()
-    .setHTML("<p>Alamo</p>")
-
-riverCenterPin.setPopup(riverCenter);
-historicalParkPin.setPopup(historicalPark);
-alamoPin.setPopup(alamo)
-
+locations.forEach(function (location) {
+    let popup = new mapboxgl.Popup()
+        .setHTML(`<p>${location.name}<br>${location.address}</p>`)
+    new mapboxgl.Marker()
+        .setLngLat(location.lngLat)
+        .addTo(map)
+        .setPopup(popup)
+})
 
 
 
